914023d378a2655d72befb3cf97161e992e549c9
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-11-04 21:10:58 -0300

    Update ia.js
    Adicionada função previsaoDaChuva
    
    Descrição:
    Esta alteração adiciona a função previsaoDaChuva ao código. A função interpreta os dados dos sensores e prevê a direção da chuva com base em condições específicas. Ela lida com diferentes combinações de sensores ativados e exibe a direção prevista no console.

--- IA/ia.js
+++ IA/ia.js
@@ -11,10 +11,13 @@ SOLUÇÃO:
 A solução envolve a implementação de um modelo de IA. Com o modelo treinado, ele é usado para fazer previsões em tempo real com base nos dados dos sensores do Arduino. 
 As previsões são então disponibilizadas em uma plataforma web para acesso dos usuários.
 
-Este código é parte de um projeto mais amplo chamado "RRT - Rain in Real Time", que visa melhorar a segurança, eficiência e qualidade de vida da usuários, fornecendo informações climáticas precisas e em tempo real. 
+Este código é parte de um projeto mais amplo chamado "RRT - Rain in Real Time", que visa melhorar a segurança, eficiência e qualidade de vida dos usuários, fornecendo informações climáticas precisas e em tempo real. 
 A implementação de inteligência artificial é fundamental para alcançar esse objetivo e proporcionar inclusão digital no cotidiano das pessoas.
 
 DATA: 02/11/2023 - xx/xx/xxxx
+
+OBSERVAÇÕES: implementação de uma inteligência artificial para prever possíveis comportamentos da chuva com base nos dados obtidos pelos sensores do Arduino. Imagine três sensores em linha; o primeiro sensor a esquerda detecta chuva; após ele, o sensor do meio detecta a chuva; isso significa que a chuva está indo para leste (para a direita). É assim que nosso programa irá deduzir a provável direção da chuva. Entretanto usaremos 5 sensores formando um quadrado. O sensor A estará no meio; o sensor B na ponta superior esquerda do quadrado; o sensor C na ponta direita superior do quadrado; o sensor D na ponta esquerda inferior do quadrado; o sensor E estará na ponta inferior direita.
+
 */
 // Biblioteca para gerar dados sintéticos. É necessário intalar a biblioteca com o comando "npm install faker". 
 const faker = require('faker');
@@ -32,14 +35,65 @@ function gerarDadosSinteticos() {
     return dadosSinteticos;
 }
 
-        let chave = alf[i];
-// Função para apresentar os dados. A função apresenta os dados para desenvolvedores.
+// Função para apresentar os dados dados para desenvolvedores.
 function apresentarDados(dados) {
-    console.log('Sensor: ', dados);
+    console.log('Sensor A: ', dados.get('A'));
+    console.log('Sensor B: ', dados.get('B'));
+    console.log('Sensor C: ', dados.get('C'));
+    console.log('Sensor D: ', dados.get('D'));
+    console.log('Sensor E: ', dados.get('E'));
+}
+
+// Main function - Função para interpretar os dados e prever a direção da chuva. 
+function previsaoDaChuva(dados) {
+
+    if (dados.get('A') === 1) {
+        if (dados.get('B') === 1) {
+            console.log('Noroeste');
+        } else if (dados.get('C') === 1) {
+            console.log('Nordeste');
+        } else if (dados.get('D') === 1) {
+            console.log('Suldoeste');
+        } else if (dados.get('E') === 1) {
+            console.log('Sudeste');
+        }
+    } else if (dados.get('B') === 1) {
+        if (dados.get('C') === 1) {
+            console.log('Leste');
+        } else if (dados.get('D') === 1) {
+            console.log('Sul');
+        } else if (dados.get('A') === 1) {
+            console.log('Sudeste');
+        }
+    } else if (dados.get('C') === 1) {
+        if (dados.get('A') === 1) {
+            console.log('Nordeste');
+        } else if (dados.get('B') === 1) {
+            console.log('Oeste');
+        } else if (dados.get('E') === 1) {
+            console.log('Sul');
+        }
+    } else if (dados.get('D') === 1) {
+        if (dados.get('B') === 1) {
+            console.log('Norte');
+        } else if (dados.get('E') === 1) {
+            console.log('Leste');
+        } else if (dados.get('A') === 1) {
+            console.log('Nordeste');
+        }
+    } else if (dados.get('E') === 1) {
+        if (dados.get('A') === 1) {
+            console.log('Noroeste');
+        } else if (dados.get('D') === 1) {
+            console.log('Oeste');
+        } else if (dados.get('C') === 1) {
+            console.log('Norte');
+        }
+    }
 }
 
 //--------------------------------------------------------------\\
 // Código principal
 gerarDadosSinteticos();
-apresentarDados(gerarDadosSinteticos());
+
 //--------------------------------------------------------------\\
